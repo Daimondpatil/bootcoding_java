c52db29112f5afd40c6f856967290671ef8e079a
Daimondpatil <daimondpatil4@gmail.com>
2022-08-18 09:37:11 +0530

    Multiplication

---
+++ bootcoding/src/com/bootcoding/basic/Multiplication.java
@@ -0,0 +1,18 @@
+package com.bootcoding.basic;
+
+public class Multiplication {
+
+    public static void main(String[] args) {
+
+        int a=10;
+        int b=20;
+        int c=a*b;
+        System.out.println(c);
+
+    }
+
+
+}
+
+
+
