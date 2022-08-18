d75da6c58af35f042c6c8580481c1a3aa2245ae9
Daimondpatil <daimondpatil4@gmail.com>
2022-08-18 09:32:27 +0530

    Addition done

---
+++ bootcoding/src/com/bootcoding/basic/Addition.java
@@ -0,0 +1,21 @@
+package com.bootcoding.basic;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class Addition {
+
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+         int c=a+b;
+
+        System.out.println(c);
+
+    }
+
+
+
+
+
+
+}
